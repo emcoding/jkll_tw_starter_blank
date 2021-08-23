74d3a8d11613f105df67c8c39236af66b6bdcf3d
Maud de Vries <emcoding@pm.me>
2021-08-23 12:54:03 +0200

    Find correct purge path 3 (using pcs3.0 syntax)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  purge: [
-    './_site/**/*.html',
-  ],
+  purge: {
+    content: ["./_site/**/*.html"],
+    css: ["./_site/css/site.css"],
+    defaultExtractor:
+        content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+},
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
