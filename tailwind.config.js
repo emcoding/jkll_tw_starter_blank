4047130342777f289c96319ff21eae0c83da2106
Maud de Vries <emcoding@pm.me>
2021-08-23 13:09:54 +0200

    Find correct purge path 3 (copying from StrpTW)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  purge: {
-    content: ["./_site/**/*.html"],
-    css: ["./_site/assets/css/main.css"],
-    defaultExtractor:
-        content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-},
+  purge: ["**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
